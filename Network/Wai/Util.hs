90697f21b5573cc89188863769346ed5c7469f49
Matt Brown <matt@softmechanics.net>
2011-02-23 00:22:05 -0800

    use an MVar to control the iteratee

--- Network/Wai/Util.hs
+++ Network/Wai/Util.hs
@@ -5,64 +5,58 @@ module Network.Wai.Util (
 import Control.Concurrent
 import Control.Concurrent.MVar
 import Control.Exception (SomeException, catch, finally)
+import Control.Monad (when)
 import Control.Monad.IO.Class
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy as L
 import Data.Enumerator (($$))
 import qualified Data.Enumerator as E
-import Data.IORef
 import Network.Wai
 import System.IO.Unsafe
 import Prelude hiding (catch)
 
 withLBS :: (L.ByteString -> IO a) -> E.Iteratee B.ByteString IO (Either SomeException a)
 withLBS f = do
-  (ialive, mbs, mres) <- liftIO $ do
+  (mcont, mbs, mres) <- liftIO $ do
     mbs <- newEmptyMVar
     mres <- newEmptyMVar
-    ialive <- newIORef True
+    mcont <- newMVar True
     forkIO $ finally 
       (catch
-        (do a <- f =<< evalLBS mbs
+        (do a <- f =<< evalLBS mcont mbs
             a `seq` putMVar mres (Right a)) 
         (putMVar mres . Left))
-      (writeIORef ialive False)
-    return (ialive, mbs, mres)
-  iterateLBS ialive mbs
+      (putMVar mcont False)
+    return (mcont, mbs, mres)
+  iterateLBS mcont mbs
   liftIO $ takeMVar mres
 
-evalLBS :: MVar [B.ByteString] -> IO L.ByteString
-evalLBS mbs = fmap L.fromChunks go
+evalLBS :: MVar Bool -> MVar [B.ByteString] -> IO L.ByteString
+evalLBS mcont mbs = fmap L.fromChunks go
   where go = unsafeInterleaveIO $ do 
                 next <- takeMVar mbs
                 case next of
                      [] -> return []
-                     bs -> fmap (bs ++) go
+                     bs -> do putMVar mcont True
+                              fmap (bs ++) go
 
-iterateLBS :: IORef Bool -> MVar [B.ByteString] -> E.Iteratee B.ByteString IO ()
-iterateLBS ialive mbs = E.continue go
+iterateLBS :: MVar Bool -> MVar [B.ByteString] -> E.Iteratee B.ByteString IO ()
+iterateLBS mcont mbs = E.continue go
   where go (E.Chunks []) = E.continue go
 
         go (E.Chunks cs) = do 
-          succ <- liftIO $ tryPut cs
+          succ <- liftIO $ waitPut cs
           if succ
              then E.continue go
              else E.yield () $ E.Chunks cs
 
         go E.EOF = do 
-          liftIO $ tryPut []
+          liftIO $ waitPut []
           E.yield () E.EOF
 
-        tryPut cs = do 
-          succ <- tryPutMVar mbs cs
-          if succ 
-             then return True
-             else waitPut cs
-
         waitPut cs = do
-          alive <- readIORef ialive
-          if alive
-             then yield >> tryPut cs
-             else return False
+          cont <- takeMVar mcont
+          when cont $ putMVar mbs cs
+          return cont
 
